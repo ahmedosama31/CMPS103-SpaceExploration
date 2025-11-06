066026ecb5a2c5c8670c075c9710fe0e35735607
ahmedosama31 <ahmed.fathy06@eng-st.cu.edu.eg>
2025-11-06 21:06:02 +0200

    Implemented All DSs from labs

---
+++ ArrayStack.h
@@ -0,0 +1,61 @@
+#pragma once
+//  created by Frank M. Carrano and Timothy M. Henry.
+//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
+
+/** ADT stack: Array-based implementation.
+ @file ArrayStack.h */
+
+#include "StackADT.h"
+
+// Unless specified by the stack user, the default size is 100
+template<typename T>
+class ArrayStack : public StackADT<T>
+{
+    enum { MAX_SIZE = 100 };
+private:
+    T items[MAX_SIZE]; // Array of stack items
+    int top;           // Index to top of stack
+
+public:
+
+    ArrayStack()
+    {
+        top = -1;
+    }  // end default constructor
+
+    bool isEmpty() const
+    {
+        return top == -1;
+    }  // end isEmpty
+
+    bool push(const T& newEntry)
+    {
+        if (top == MAX_SIZE - 1) return false; // Stack is FULL
+
+        top++;
+        items[top] = newEntry;
+        return true;
+    }  // end push
+
+    bool pop(T& TopEntry)
+    {
+        if (isEmpty()) return false;
+
+        TopEntry = items[top];
+        top--;
+        return true;
+    }  // end pop
+
+    bool peek(T& TopEntry) const
+    {
+        if (isEmpty()) return false;
+
+        TopEntry = items[top];
+        return true;
+    }  // end peek
+
+    int getCount() const
+    {
+        return top + 1;
+    }
+}; // end ArrayStack
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "priQueue.h"
+
+int main()
+{
+ 
+    return 0;
+}
