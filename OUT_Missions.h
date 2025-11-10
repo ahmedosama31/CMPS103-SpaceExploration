5a5d89727920e2ef6570656cc03fb96db5679beb
ahmedosama31 <ahmed.fathy06@eng-st.cu.edu.eg>
2025-11-10 08:49:37 +0200

    Abortmission function in OUT_missions and set head in priQueue to protected

--- OUT_Missions.h
+++ OUT_Missions.h
@@ -1,8 +1,35 @@
 #pragma once
 #include "priQueue.h"
 #include "mission.h"
-class OUT_Missions : public priQueue<mission*>
+class OUT_missions : public priQueue<mission*>
 {
 public:
-	mission* AbortMission(int ID) {}
+	mission* Abortmission(int ID) 
+	{
+		if (this->isEmpty()) 
+			return nullptr;
+
+
+		priNode<mission*>* previous = nullptr;
+		priNode<mission*>* current = this->head;
+
+
+		while (current) {
+			int pri;
+			mission* m = current->getItem(pri);
+			if (m->getID() == ID) {
+				if (current == this->head)
+					this->head = current->getNext();
+				else
+					previous->setNext(current->getNext());
+
+
+				delete current; // delete node only
+				return m;
+			}
+			previous = current;
+			current = current->getNext();
+		}
+		return nullptr;
+	}
 };
